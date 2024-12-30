0891666f74cf87184335d78f44c3059fbb7aa023
kaydeesea <comemache@gmail.com>
2024-12-30 09:43:08 +0100

    d

--- kewlspigot-api/src/main/java/org/eytril/spigot/asm/CustomClassLoader.java
+++
@@ -1,6 +0,0 @@
-package org.eytril.spigot.asm;
-public class CustomClassLoader extends ClassLoader {
-    public Class<?> defineClass(String name, byte[] bytecode) {
-        return super.defineClass(name, bytecode, 0, bytecode.length);
-    }
-}
--- kewlspigot-server/src/main/java/org/eytril/spigot/util/BooleanUtil.java
+++
@@ -1,9 +0,0 @@
-package org.eytril.spigot.util;
-
-public class BooleanUtil {
-
-    public static boolean isBoolean(String s) {
-        return s.equalsIgnoreCase("true") || s.equalsIgnoreCase("false");
-    }
-
-}
